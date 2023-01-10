c9b8c575b02c02c0901832819369326df6204fdd
Mert <97520268+mertkama@users.noreply.github.com>
2023-01-10 16:35:03 +0300

    Create stringInterpolation.js

---
+++ stringInterpolation.js
@@ -0,0 +1,8 @@
+// stringInterpolation
+
+const name = 'Mert'; 
+const job = 'developer'; 
+const newUserInfo = `Hi my name is ${name} and I am a ${job}.`; 
+console.log(newUserInfo);
+
+// Output : Hi my name is Mert and I am a developer.
