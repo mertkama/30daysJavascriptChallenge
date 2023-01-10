2be3d43c1af3be11554164f19e38f55be3a97a73
Mert <97520268+mertkama@users.noreply.github.com>
2023-01-10 15:07:36 +0300

    Create kelvinWeather.js

---
+++ kelvinWeather.js
@@ -0,0 +1,13 @@
+// Current temperature in kelvin degrees.
+const kelvin = 0;
+// Converting kelvin to celsius.
+const celsius = kelvin - 273;
+// Convert celsius to fahrenheit.
+let fahrenheit = celsius * (9/5) + 32;
+// Round down fahrenheit variable.
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature ${fahrenheit} degrees Fahrenheit.`);
+
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature in ${newton} degrees Newton.`);
