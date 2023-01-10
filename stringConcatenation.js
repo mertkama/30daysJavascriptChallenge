e4b336cb0649eef7ae5d97632097089db4215547
Mert <97520268+mertkama@users.noreply.github.com>
2023-01-10 16:26:48 +0300

    Update stringConcatenation.js

--- stringConcatenation.js
+++ stringConcatenation.js
@@ -1,5 +1,8 @@
 // String Concatenation
 
-const name = "Mert"; const age = 23; const combined = name + age; console.log(combined);
+const name = "Mert"; 
+const age = 23; 
+const combined = name + age; 
+console.log(combined);
 
 // Output Mert23
